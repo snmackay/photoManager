2a7b1beb3845646b83c824f8f373c285d3a9d9e0
Sean <seannmackay@gmail.com>
2019-08-09 19:05:49 -0400

    Fixed and removed jpg.exif support.

--- photoManager.py
+++ photoManager.py
@@ -4,9 +4,10 @@ import sys
 from os import listdir
 from os.path import isfile, join
 from PIL import Image
+import shutil
 
 
-
+#_______________________________________________________________________________
 #generates list of photos within directory
 def genFileList(directory):
     listOfFiles=os.listdir(directory)
@@ -19,24 +20,66 @@ def genFileList(directory):
             photoFiles=photoFiles+[file]
 
     return photoFiles
+#_______________________________________________________________________________
 
+#_______________________________________________________________________________
 #checks to see if exif data can be used
-def getFileDate(photo):
-    varr=Image.open(photo)._getexif()[36867]
-    if varr
+#def getFileDate(photo):
+    #varr=Image.open(photo)._getexif()[36867]
+    #if varr==None:
+        #return photo
+    #else:
+        #return varr
+    #return photo
+#_______________________________________________________________________________
 
-#-------------------------------------------------------------------------------
+#_______________________________________________________________________________
 #for files with exif data:
-#-------------------------------------------------------------------------------
 #renames the files that are being passed in.
-def renameFile(file,date):
+#def renameFileExif(file,date):
     #print(date)
-    date2=date.split( )
-    date2="Year-"+date2[0]+" Time-"+date2[1]
-    date2=date2.replace(":" , "_")
-    os.rename(file,date2+".jpg")
-    return date2+".jpg"
+    #date2=date.split( )
+    #date2="Year-"+date2[0]+" Time-"+date2[1]
+    #date2=date2.replace(":" , "_")
+    #os.rename(file,date2+".jpg")
+    #return date2+".jpg"
+#_______________________________________________________________________________
 
+#_______________________________________________________________________________
+#for files without exif data
+#renames files that are being passed in
+def renameFileOOF(file):
+    if("IMG_") in file:
+        file2=file.split("_")
+        yearNums=file2[1]
+        year="Year-"+yearNums[0:4]+"_"+yearNums[4:6]+"_"+yearNums[6:8]
+        year2=year+" Time-"+file2[2]
+        os.rename(file,year2+".jpg")
+        print("if1")
+        return year2+".jpg"
+    else :
+        file2=file.split("_")
+        print(file2)
+        if(file2[0].isnumeric()):
+            yearNums=file2[0]
+            year="Year-"+yearNums[0:4]+"_"+yearNums[4:6]+"_"+yearNums[6:8]
+            if(file2[1].isnumeric()):
+                year2=year+" Time-"+file2[1]
+                year2=year2+file2[1]
+                os.rename(file,year2+".jpg")
+                return year2+".jpg"
+            else:
+                year2=year+" Time-"+file2[1]
+                year2=year2+file2[1]
+                os.rename(file,year2+".jpg")
+                return year2+".jpg"
+        else:
+            return "Other"+file
+
+#_______________________________________________________________________________
+
+#_______________________________________________________________________________
+#restore the photos in a file struct
 def storePhoto(newfile):
 
     #setup stuff
@@ -60,6 +103,7 @@ def storePhoto(newfile):
     months['12']="December"
 
     #create year directory
+
     newPath=os.path.join(currentPath,year,months[month])
     try:
         os.makedirs(newPath)
@@ -71,27 +115,23 @@ def storePhoto(newfile):
     #move the file
     current=os.path.join(currentPath,newfile)
     newDir=os.path.join(newPath,newfile)
-    os.rename(current,newDir)
+    shutil.move(current,newDir)
+#_______________________________________________________________________________
 
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-
-
+#_______________________________________________________________________________
 #main controller function, calls helpers in order
 def main(directory):
     photoFiles=genFileList(directory)
 
     for file in photoFiles:
-        date=getFileDate(file)
-        #print(file)
-        newfile=renameFile(file,str(date))
+        print(file)
+        #date=getFileDate(file)
+        newfile=renameFileOOF(file)
 
         storePhoto(newfile)
 
-    #renameFiles(photoFiles)
-    #fixedFiles=genFileList(directory)
 
-
+#_______________________________________________________________________________
 if "__name__==__main__":
     inputer= os.getcwd()
     main(inputer)
