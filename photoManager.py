8d1767d1fe4c343627118a9b18c658599237221f
Sean <seannmackay@gmail.com>
2019-08-08 21:24:56 -0400

    photoManager.py now renames the files

--- photoManager.py
+++ photoManager.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 from os import listdir
 from os.path import isfile, join
+from PIL import Image
 
 #generates list of photos within directory
 def genFileList(directory):
@@ -11,22 +12,38 @@ def genFileList(directory):
 
     #Iterate over all files in directory and grab just photos
     for file in listOfFiles:
-        fullPath=os.path.join(directory,entry)
+        fullPath=os.path.join(directory,file)
         if fullPath.endswith(".jpg") or fullPath.endswith(".png"):
-            photoFiles=photoFiles+file
+            photoFiles=photoFiles+[file]
 
     return photoFiles
 
-def renameFiles(photoFiles):
+#gets file date taken using EXIF
+def getFileDate(photo):
+    return Image.open(photo)._getexif()[36867]
+
+#renames the files that are being passed in.
+def renameFile(file,date):
+    print(date)
+    date2=date.split( )
+    date2="Day-"+date2[0]+" Time-"+date2[1]
+    date2=date2.replace(":" , "_")
+    os.rename(file,date2+".jpg")
 
 
 #main controller function, calls helpers in order
 def main(directory):
     photoFiles=genFileList(directory)
-    renameFiles(photoFiles)
-    fixedFiles=genFileList(directory)
+
+    for file in photoFiles:
+        date=getFileDate(file)
+        print(date)
+        renameFile(file,str(date))
+
+    #renameFiles(photoFiles)
+    #fixedFiles=genFileList(directory)
 
 
 if "__name__==__main__":
-    inputer= str(sys.argv[1])
+    inputer= os.path.dirname(os.path.realpath(__file__))
     main(inputer)
